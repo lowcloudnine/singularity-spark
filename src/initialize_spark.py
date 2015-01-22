cff6638806454164975c44bff3bbe6076abb3e91
Jon Schiefelbein <schiefjm@c2-master.ec2.internal>
2015-01-22 22:22:38 +0000

    Inserted first working test code and added a description in docs.

---
+++ src/initialize_spark.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Purpose
+-------
+
+A centralized file for initializing 'sc' for using in Python programs that 
+run on the Spark Server.
+
+"""
+# ----------------------------------------------------------------------------
+#    Imports
+# ----------------------------------------------------------------------------
+
+
+# ---- PySpark Library
+import pyspark
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+# ----------------------------------------------------------------------------
+#    Functions
+# ----------------------------------------------------------------------------
+
+
+def get_sc(app_name="My App", memory="2G", cores="32"):
+    """  Returns a SparkContext with the configuration for a specific
+    Spark Server.
+
+    """
+    conf = SparkConf()
+    conf.setMaster("spark://c2-master.ec2.internal:7077")
+    conf.setAppName(app_name)
+    conf.set("spark.executor.memory", memory)
+    conf.set("spark.cores.max", cores)
+
+    return SparkContext(conf=conf)
---
+++ src/py_csv_load.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+""" 
+Purpose
+-------
+
+Use spark-submit to load weather gsod files and see if I can play with the
+output.
+
+"""
+# ----------------------------------------------------------------------------
+#    Imports
+# ----------------------------------------------------------------------------
+
+
+# ---- Future Functions
+from __future__ import print_function
+
+# ---- Standard Libraries
+import csv
+import StringIO
+
+# ---- PySpark Libraries
+import pyspark
+from pyspark import SparkConf
+from pyspark import SparkContext
+
+# sc defined global as there can be only one
+# -- there needs to be a better way to do this
+sc = SparkContext(conf=SparkConf())
+
+# ----------------------------------------------------------------------------
+#    Functions 
+# ----------------------------------------------------------------------------
+
+
+def global_mean_temp(year):
+    """ Calcuates the mean temperature from the mean temperature from all
+    the observations for a given year from 1929 to 2009 inclusive. 
+
+    """
+    server_name = "hdfs://c2-master:8020/"
+    dir_name = "user/schiefjm/weather/gsod/"
+    year_name = str(year)
+
+    obs_rdd = sc.textFile(server_name + dir_name + year_name)
+    num_obs = obs_rdd.count()
+    sum_mean_temps = obs_rdd.filter(lambda line: "STN" not in line)\
+                            .map(lambda line: line.split()[3])\
+                            .map(lambda value: float(value))\
+                            .reduce(lambda x, y: x + y)
+    return round(sum_mean_temps / num_obs, 2)
+
+# ----------------------------------------------------------------------------
+#    Main 
+# ----------------------------------------------------------------------------
+
+
+def main():
+    """ Run the script as a stand alone application. """
+    mean_temps = [[year, global_mean_temp(year)] 
+                    for year in xrange(1929, 1949 + 1)] 
+    for mean_temp in mean_temps:
+        print("{}:  {}".format(mean_temp[0], mean_temp[1])) 
+
+# ----------------------------------------------------------------------------
+#    Name 
+# ----------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+	main()
